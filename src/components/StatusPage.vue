bae58885d594c591c9c444f5d66c95601e71bed8
Webrating <32590926+webrating@users.noreply.github.com>
2020-06-27 10:22:23 +1000

    Update StatusPage.vue

--- src/components/StatusPage.vue
+++ src/components/StatusPage.vue
@@ -65,7 +65,7 @@
                 >
                   <div class="uptime">
                     <p class="text-muted desc">
-                      Response Time
+                      Avg Response
                     </p>
                     <span>{{ thisCheck.response + 'ms' }}</span>
                   </div>
